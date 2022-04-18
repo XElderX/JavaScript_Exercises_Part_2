a1bc8b89147209b6d2831f65cb279da65d303d39
XElderX <dalius.badley@gmail.com>
2022-04-19 00:06:48 +0300

    Part 2 of JS exercises #1 n # 2 completed

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # JavaScript_Exercises_Part_2
 The 2nd part of JS course:  functions, recursive functions, advanced array methods
+###
 
 
-// #1 UŽDUOTIS: Sukurkite funkciją leftPad, kuri padavus stringą ir plotį, kuriame jis turi išsitekti išspausdina stringą dešinėje tarkim: leftPad(text, width) 
-// :  leftPad(‘ab’, 4) → |__ab| 
-#2.Uždaviniai:
-// faktorialas neigiamiems skaičiams matematiškai nėra apibrėžtas, bet google kalkuliatorius suskaičiuoja! 5! ⇒ -120 ⇒ -(5)! (triukas!). Pamofikuokite klasėje matytą
-//  factorial funkciją, kad veiktų taip kaip google kalkuliatorius. 
-#3užduotis
-//  padarykite tyrimą - internete raskite rekursyvios funkcijos pavyzdį, kuris jums aiškiausias. Parašykite kodą, kuris jums geriausiai pailiustruotų jos veikimą - parenkite pavyzdį sau pačiam. Nepamirškite, jog pavyzdys, turi būti taip parašytas,
-//   jog po mėnesio sugrįžus būtų visiškai aišku kaip, kas, kam ir kodėl. sužinokite ar galima parašyti arraySum() rekursyviai? Ir jei taip - parašykite, jei ne, pasakykite kodėl.
+<!-- #1 Uždavinys:
+matėme, kaip apsirašyti objektų šablonus - klases. Pabandykime sukurti objektus, kurie turi kitus objektus kaip savybes, naudosime jau matytą Employees ir Departments pavyzdį, tik dabar kursime Department objektą ir priskirsime darbuotojus.  -->
+
+
+
+<!-- #2 uzdavinys. sukurkime klases Car { make, model, engine } ir Engine { horsePower, displacement }. Car klasė turės metodą getPrice(), kuris gražins automobilio kainą, kuri bus skaičiuojama pagal formulę: bazinėKaina * horsePower + (bazinėKaina / 2 *displacement).
+ -->
--- excercises2.js
+++ excercises2.js
@@ -1,45 +1,73 @@
-// #1 UŽDUOTIS: Sukurkite funkciją leftPad, kuri padavus stringą ir plotį, kuriame jis turi išsitekti išspausdina stringą dešinėje tarkim: leftPad(text, width) 
-// :  leftPad(‘ab’, 4) → |__ab| 
+// #1 Uždavinys:
+//  Pabandykime sukurti objektus, kurie turi kitus objektus kaip savybes, naudosime jau matytą Employees ir Departments pavyzdį,
+//  tik dabar kursime Department objektą ir priskirsime darbuotojus. 
 
-/* function leftPad (text,width){
-    if (text.length>=width){
-        return `${text}`
-    }
-    else {
-        return `_${leftPad(text,width-1)}`
-    }
+/* class Department  {
+    constructor(_id, _name){
+        this.id = _id;
+        this.name = _name;
+        this.employees = [];
+    };
+    addEmployee(employee){
+        this.employees.push(employee);
+    } 
+
 }
-console.log(leftPad("labas",7)) */
 
-// #2.Uždaviniai:
-// faktorialas neigiamiems skaičiams matematiškai nėra apibrėžtas, bet google kalkuliatorius suskaičiuoja! 5! ⇒ -120 ⇒ -(5)! (triukas!). Pamofikuokite klasėje matytą
-//  factorial funkciją, kad veiktų taip kaip google kalkuliatorius.
+let managament = new Department(0, "Managament");
+let marketing = new Department(1, "Marketing");
+let sales = new Department(2, "Sales");
+managament.addEmployee("Robin");
+managament.addEmployee("Wilson");
+marketing.addEmployee("Garikas");
+marketing.addEmployee("Klaudija");
+sales.addEmployee("Samana");
+sales.addEmployee("Onute");
+
+console.log(managament);
+console.log(marketing);
+console.log(sales); */
 
 
-/* let factorial = (number) => {
-    if(number<0){
-        for(i=number+1; i<0; i++){
-            number*=(i)
+// #2 uzdavinys sukurkime klases Car { make, model, engine } ir Engine { horsePower, displacement }. Car klasė turės metodą
+//   getPrice(), kuris gražins automobilio kainą, kuri bus skaičiuojama pagal formulę: bazinėKaina * horsePower + (bazinėKaina / 2 *displacement).
+
+
+class Car {
+    constructor(_brand, _model, _engine){
+        this.brand = _brand;
+        this.model = _model;
+        this.engine = _engine;
+        this.carBodyPrice = 400;
+    }
+    getPrice(){
+        return `Visa kaina: ${this.carBodyPrice * this.engine.horsePower + (this.carBodyPrice / 2 * this.engine.engineDisplacement)} € `
+    }
+    }
+    class Engine {
+        constructor(_power, _displacement){
+            this.horsePower = _power;
+            this.engineDisplacement = _displacement;
         }
-        return number
     }
-    for(i=number-1; i>0; i--){
-        number*=(i)
-    }
-    return number
-    }
-console.log(factorial(-5))
- */
-// #3 užduotis padarykite tyrimą - internete raskite rekursyvios funkcijos pavyzdį, kuris jums aiškiausias.  Parašykite kodą, kuris jums geriausiai pailiustruotų jos veikimą - 
-//parenkite pavyzdį sau pačiam. Nepamirškite, jog pavyzdys, turi būti taip parašytas, jog po mėnesio sugrįžus būtų visiškai aišku kaip, kas, kam ir kodėl. sužinokite ar galima
-// parašyti arraySum() rekursyviai? Ir jei taip - parašykite, jei ne, pasakykite kodėl.
+    let engineV1 = new Engine(91, 1.9);
+    let engineV1Turbo = new Engine(103, 1.9);
+    let engineV2 = new Engine(122, 2.0);
+    let engineV2x = new Engine(132, 2.0);
+    let engineNex9s = new Engine(277, 3.5);
 
-/* let sumFactorial = function(number){
-   if (number<=1){
-       return number 
-   }
-  return number + sumFactorial(number-1)
-    
 
-}
-console.log(sumFactorial(7)) */
+    let car1 = new Car("Audi","RS", engineV2x)
+    let car2 = new Car("Jeep","XRC", engineNex9s)
+    let car3 = new Car("VW","Golf", engineV1)
+    let car4 = new Car("Volvo","XREW", engineV2)
+
+
+    console.log(car1)
+    console.log(car1.getPrice())
+    console.log(car2)
+    console.log(car2.getPrice())
+    console.log(car3)
+    console.log(car3.getPrice())
+    console.log(car4)
+    console.log(car4.getPrice())
